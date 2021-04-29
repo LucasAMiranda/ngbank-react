0982d2af0ef327838bdb5ef0d973997c5efd3f7f
LucasAMiranda <lucasa.miranda@hotmail.com>
2021-04-28 21:11:55 -0300

    Iniciando Projeto

---
+++ ngbank/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />     
+      
+       <h1> Bem vindo ao react</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ ngbank/src/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Header(){
+
+    return(<h1>Componente do header</h1>)
+}
+
+export default Header;
